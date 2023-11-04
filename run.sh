bfaaee2d2ebe3c857dd297294ff1e79dcaa06190
Arash Yadegari <aryadegari@gmail.com>
2023-11-04 14:59:19 +0100

    add run.sh and update gitignore for temp files

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the path to your virtual environment's activate script
+venv_activate=".venv/bin/activate"
+
+# Check if the virtual environment activation script exists
+if [ -f "$venv_activate" ]; then
+    # Activate the virtual environment
+    source "$venv_activate"
+
+    # Run your Python script
+    python main.py
+else
+    echo "Virtual environment not found or activation script missing."
+fi
